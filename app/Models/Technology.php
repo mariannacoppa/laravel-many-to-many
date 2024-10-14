06fe70ee9a2babbf976be8ba517b552a545ad36d
mariannacoppa <densimetra@gmail.com>
2024-10-14 16:39:30 +0200

    [UPD] set methods and table fields, then launched migration

--- app/Models/Technology.php
+++ app/Models/Technology.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Technology extends Model
 {
     use HasFactory;
+    protected $fillable = ['name', 'slug'];
+
+    // realizzo i metodi 
+
+    // per generare lo slug
+    public static function generateSlug($title){
+        return Str::slug($title, '-');
+    }
+
+    public function projects(){
+        return $this->belongsToMany(Project::class);
+    }
 }
